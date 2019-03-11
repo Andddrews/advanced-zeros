64841b2f2bf1dd79a6828ed50c8d0ae3e872a514
Andrei Savastsijonak <Andddrews@gmail.com>
2019-03-11 17:43:52 +0300

    37

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ module.exports = function getZerosCount(number, base) {
   let p = 2;
   let b = base;
   while (b%p) {
-  b =(b/p) ;
-  p++;
- 
-  }; 
+   p++;
+   }; 
+  
  
   //alert(p);
-  while (number/Math.pow(p,k)>1){
+  while (number/Math.pow(p,k)>=1){
 k=k+1;
-
   }
-  
+  k=k-1;
+
   //alert(k);
 let m = Math.floor(Math.log(base) / Math.log(p));
 
 //alert(m);
 
 let result = 0;
- for(let i=1; i<k; i++){
+ for(let i=1; i<=k; i++){
    result = result + Math.floor(number/Math.pow(p,i))
  }
  //alert(result);
